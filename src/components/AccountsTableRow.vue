d78d3bf9b7be238f9dbedc1290335d7dbb5e36c3
ttempaa <ttempaa.mt@gmail.com>
2025-07-07 01:11:52 +0300

    feat: add validation before save

--- src/components/AccountsTableRow.vue
+++ src/components/AccountsTableRow.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import { reactive, ref, watch } from 'vue';
+import { reactive, ref } from 'vue';
 import { NInput, NSelect } from 'naive-ui'
 import { useAccountsStore } from '@/stores/accounts'
 import { AccountType, type Account } from '@/types/account';
 import RemoveAccountButton from '@/components/RemoveAccountButton.vue'
 
 const marksSeparator = ';'
-
-const props = defineProps<{ account: Account }>()
-const store = useAccountsStore()
-const localAccount = reactive({ ...props.account })
-const stringMarks = ref(localAccount.marks.map(mark => mark.text).join(`${marksSeparator} `))
-
 const accountTypeSelectOptions = [
   {
     value: AccountType.LOCAL,
@@ -23,39 +17,55 @@ const accountTypeSelectOptions = [
   }
 ]
 
-function prepareAndUpdate(newValue: Account) {
-  newValue.marks = stringMarks.value.split(marksSeparator).map((mark) => ({ text: mark.trim() }))
-  if (newValue.type === AccountType.LDAP) {
-    localAccount.password = "";
-    newValue.password = null;
+const props = defineProps<{ account: Account }>()
+const store = useAccountsStore()
+const localAccount = reactive({ ...props.account })
+const stringMarks = ref(localAccount.marks.map(mark => mark.text).join(`${marksSeparator} `))
+
+function areInputsValid(): boolean {
+  return localAccount.login.length > 0 && (
+    localAccount.type === AccountType.LOCAL
+      ? localAccount.password !== null && localAccount.password.length > 0
+      : true
+  )
+}
+
+function prepareValues() {
+  localAccount.marks = stringMarks.value.split(marksSeparator).filter(Boolean).map((mark) => ({ text: mark.trim() }))
+  if (localAccount.type === AccountType.LDAP) {
+    localAccount.password = ""
+    localAccount.password = null
   }
+}
+
+function save() {
+  if (!areInputsValid()) {
+    return
+  }
+  prepareValues()
   store.updateAccount(props.account.id, {
-    ...newValue,
+    ...localAccount,
     id: props.account.id,
   })
 }
-
-watch(
-  () => ({ ...localAccount, stringMarks }),
-  (newValue) => prepareAndUpdate(newValue),
-  { deep: true }
-)
 </script>
 
 <template>
   <tr>
     <td>
-      <n-input v-model:value="stringMarks" type="text" placeholder="Значение" />
+      <n-input v-model:value="stringMarks" @blur="save()" type="text" placeholder="Значение" maxlength="50" />
     </td>
     <td>
-      <n-select v-model:value="localAccount.type" :options="accountTypeSelectOptions" />
+      <n-select v-model:value="localAccount.type" @update:value="save()" :options="accountTypeSelectOptions" />
     </td>
     <td>
-      <n-input v-model:value="localAccount.login" type="text" placeholder="Значение" />
+      <n-input v-model:value="localAccount.login" @blur="save()" type="text" placeholder="Значение" maxlength="100"
+        :status="localAccount.login.length > 0 ? 'success' : 'error'" />
     </td>
     <td>
-      <n-input v-if="localAccount.type === AccountType.LOCAL" v-model:value="localAccount.password" type="password"
-        placeholder="Значение" />
+      <n-input v-if="localAccount.type === AccountType.LOCAL" v-model:value="localAccount.password" @blur="save()"
+        type="password" placeholder="Значение" maxlength="100"
+        :status="localAccount.password !== null && localAccount.password.length > 0 ? 'success' : 'error'" />
     </td>
     <td>
       <remove-account-button :account-id="account.id" />
