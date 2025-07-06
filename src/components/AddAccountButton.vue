7e235bc5df6cd84eca5615f8d5d2db99afb37067
ttempaa <ttempaa.mt@gmail.com>
2025-07-06 16:31:37 +0300

    feat: create `AddAccountButton` component

---
+++ src/components/AddAccountButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { NButton, NIcon } from 'naive-ui'
+import { Add12Filled } from '@vicons/fluent'
+import { useAccountsStore } from '@/stores/accounts';
+import { AccountType } from '@/types/account';
+
+const store = useAccountsStore()
+
+function handleClick() {
+  store.addAccount({
+    marks: [],
+    type: AccountType.LOCAL,
+    login: '',
+    password: ''
+  });
+}
+</script>
+
+<template>
+  <n-button @click="handleClick">
+    <template #icon>
+      <n-icon>
+        <Add12Filled />
+      </n-icon>
+    </template>
+  </n-button>
+</template>
