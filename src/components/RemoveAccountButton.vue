08b8b615fc98d614a8f536586b262b4434439daf
ttempaa <ttempaa.mt@gmail.com>
2025-07-07 00:04:03 +0300

    feat: create `RemoveAccountButton` component

---
+++ src/components/RemoveAccountButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { NButton, NIcon } from 'naive-ui'
+import { Delete16Filled } from '@vicons/fluent'
+import { useAccountsStore } from '@/stores/accounts';
+
+const props = defineProps<{ accountId: string }>()
+
+const store = useAccountsStore()
+
+function handleClick() {
+  store.removeAccount(props.accountId);
+}
+
+</script>
+
+<template>
+  <n-button @click="handleClick">
+    <template #icon>
+      <n-icon>
+        <Delete16Filled />
+      </n-icon>
+    </template>
+  </n-button>
+</template>
