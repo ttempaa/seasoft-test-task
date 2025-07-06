d5396b1831192db297cd78806f2544de8b75deae
ttempaa <ttempaa.mt@gmail.com>
2025-07-07 00:07:12 +0300

    feat: setup main app component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { NAlert, NSpace, NLayout, NLayoutHeader, NLayoutContent, NH1 } from 'naive-ui'
+import AccountsTable from '@/components/AccountsTable.vue';
+import AddAcccountButton from '@/components/AddAccountButton.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <n-layout>
+    <n-layout-header>
+      <n-h1>
+        Учетные записи
+        <AddAcccountButton />
+      </n-h1>
+    </n-layout-header>
+    <n-layout-content>
+      <n-space vertical>
+        <n-alert type="info">
+          Для указания нескольких меток для одной пары логин/пароль используйте разделитель
+          <code>;</code>
+        </n-alert>
+        <accounts-table />
+      </n-space>
+    </n-layout-content>
+  </n-layout>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
